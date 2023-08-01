55de0d270c5f063a66c85272d7398f5db72d6304
MyCode07 <erem19992015@mail.ru>
2023-08-01 16:13:13 +0300

    add gallery

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,11 @@
+import { Fancybox } from "@fancyapps/ui";
+
+Fancybox.bind("[data-fancybox]", {
+    // Your custom options
+});
+
+
+
 import './utils/menu.js';
 import './utils/sliders.js';
 import './utils/to-top.js';
@@ -6,7 +14,8 @@ import './utils/inputmask.js';
 import './utils/filter.js';
 import './utils/audit.js';
 import './utils/accordeon.js';
- 
+
+
 // import './utils/smoothscrol.js';
 
 // import './utils/mouse.js';
